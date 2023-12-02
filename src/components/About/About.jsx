2c7a28ecb94232ca15469283c01aaacba68a8794
Janet Webster <twixmixy@gmail.com>
2023-12-02 13:41:52 -0500

    Add About component with styling

---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MDBContainer, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage } from 'mdb-react-ui-kit';
+import './About.css';
+import headshot from '../../assets/images/headshot.JPEG';
+
+const About = () => {
+    return (
+        <MDBContainer fluid className='vh-100 bkgrd'>
+            <div className="d-flex justify-content-center flex-column flex-sm-row">
+                <MDBCard className="order-sm-2 card-picture my-4 mx-auto mx-sm-0">
+                    
+                </MDBCard>
+            </div>
+        </MDBContainer>
+    )
+}
