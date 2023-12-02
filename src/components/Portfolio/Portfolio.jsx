2cf68fe1ebb142ba11809fa0634d0dcf18af63a7
Janet Webster <twixmixy@gmail.com>
2023-12-02 14:48:57 -0500

    Add portfolio component with card layout

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { 
+    MDBContainer,
+    MDBRow,
+    MDBCard,
+    MDBCol,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImage,
+    MDBCardLink,
+    MDBCardFooter
+} from 'mdb-react-ui-kit';
+import project1 from '../../assets/project1.png';
+import project2 from '../../assets/project2.png';
+import project3 from '../../assets/project3.png';
+import project4 from '../../assets/project4.png';
+import project5 from '../../assets/project5.png';
+import project6 from '../../assets/project6.png';
+
+const Portfolio = () => {}
