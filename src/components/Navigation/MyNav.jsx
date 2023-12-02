c69bb061ac690b1fcfe57bd50cfe1b12ed2135e9
Janet Webster <twixmixy@gmail.com>
2023-12-02 12:55:10 -0500

    Add MyNav component to project with their imports

---
+++ src/components/Navigation/MyNav.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { 
+    MDBNavbar,
+    MDBContainer,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBCollapse,
+    MDBIcon,
+    MDBNavbarNav
+} from 'mdb-react-ui-kit';
+import { LinkContainer } from 'react-router-bootstrap';
+import './MyNav.css';
+
+const MyNav = () => {
+
+}
+
+export default MyNav;
