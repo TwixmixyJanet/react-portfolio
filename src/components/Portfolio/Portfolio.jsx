369230a484ef9b79a0d00d297beca53f4fe03006
Janet Webster <twixmixy@gmail.com>
2023-12-02 15:00:12 -0500

    Add portfolio card layout

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -18,4 +18,109 @@ import project4 from '../../assets/project4.png';
 import project5 from '../../assets/project5.png';
 import project6 from '../../assets/project6.png';
 
-const Portfolio = () => {}
+const Portfolio = () => {
+    return (
+        <MDBContainer fluid className="h-100 pb-4 bkgrd">
+            <MDBRow className="row-cols-1 row-cols-md-3 g-3 pt-2">
+                <MDBCol>
+                    <MDBCard className="h-100">
+                        <MDBCardImage className="img-thumbnail" position='top' src={project1}/>
+                        <MDBCardBody>
+                            <MDBCardTitle>Project 1</MDBCardTitle>
+                            <MDBCardText>
+                                <p>Project 1 description</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                            </MDBCardText>
+                        </MDBCardBody>
+                        <MDBCardFooter className="text-center">
+                            <MDBCardLink href=''>Repo Link</MDBCardLink>
+                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                        </MDBCardFooter>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard className="h-100">
+                        <MDBCardImage className="img-thumbnail" position='top' src={project1}/>
+                        <MDBCardBody>
+                            <MDBCardTitle>Project 1</MDBCardTitle>
+                            <MDBCardText>
+                                <p>Project 1 description</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                            </MDBCardText>
+                        </MDBCardBody>
+                        <MDBCardFooter className="text-center">
+                            <MDBCardLink href=''>Repo Link</MDBCardLink>
+                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                        </MDBCardFooter>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard className="h-100">
+                        <MDBCardImage className="img-thumbnail" position='top' src={project1}/>
+                        <MDBCardBody>
+                            <MDBCardTitle>Project 1</MDBCardTitle>
+                            <MDBCardText>
+                                <p>Project 1 description</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                            </MDBCardText>
+                        </MDBCardBody>
+                        <MDBCardFooter className="text-center">
+                            <MDBCardLink href=''>Repo Link</MDBCardLink>
+                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                        </MDBCardFooter>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard className="h-100">
+                        <MDBCardImage className="img-thumbnail" position='top' src={project1}/>
+                        <MDBCardBody>
+                            <MDBCardTitle>Project 1</MDBCardTitle>
+                            <MDBCardText>
+                                <p>Project 1 description</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                            </MDBCardText>
+                        </MDBCardBody>
+                        <MDBCardFooter className="text-center">
+                            <MDBCardLink href=''>Repo Link</MDBCardLink>
+                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                        </MDBCardFooter>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard className="h-100">
+                        <MDBCardImage className="img-thumbnail" position='top' src={project1}/>
+                        <MDBCardBody>
+                            <MDBCardTitle>Project 1</MDBCardTitle>
+                            <MDBCardText>
+                                <p>Project 1 description</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                            </MDBCardText>
+                        </MDBCardBody>
+                        <MDBCardFooter className="text-center">
+                            <MDBCardLink href=''>Repo Link</MDBCardLink>
+                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                        </MDBCardFooter>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard className="h-100">
+                        <MDBCardImage className="img-thumbnail" position='top' src={project1}/>
+                        <MDBCardBody>
+                            <MDBCardTitle>Project 1</MDBCardTitle>
+                            <MDBCardText>
+                                <p>Project 1 description</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                            </MDBCardText>
+                        </MDBCardBody>
+                        <MDBCardFooter className="text-center">
+                            <MDBCardLink href=''>Repo Link</MDBCardLink>
+                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                        </MDBCardFooter>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
+    )
+};
+
+export default Portfolio;
