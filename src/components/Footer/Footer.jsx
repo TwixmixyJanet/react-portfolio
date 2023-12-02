648d696190f5e75f7bb83c28a626896382186b4e
Janet Webster <twixmixy@gmail.com>
2023-12-02 14:35:32 -0500

    Add social media links to Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,15 @@ const Footer = () => {
         <MDBFooter className="text-center text-lg-start text-muted" bgColor='light'>
             <div className="text-center p-4" style={{ backgroundColor: 'rgba(0,0,0,0.05' }}>
                 <span>
-                    
+                    <a rel='noreferrer' href="https://www.linkedin.com/in/twixmixy/" className="me-4 text-reset" target='_blank'>
+                        <MDBIcon fab icon='linkedin' size='2x'/>
+                    </a>
+                    <a rel='noreferrer' href="https://github.com/TwixmixyJanet" className="me-4 text-reset" target='_blank'>
+                        <MDBIcon fab icon='github' size='2x'/>
+                    </a>
+                    <a rel='noreferrer' href="https://twixmixy.substack.com/" className="me-4 text-reset" target='_blank'>
+                        <MDBIcon fab icon='newspaper' size='2x'/>
+                    </a>
                 </span>
             </div>
         </MDBFooter>
