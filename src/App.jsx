191c3755e476cefec5462f53dbce1d1f2bc02096
Janet Webster <twixmixy@gmail.com>
2023-12-02 11:22:04 -0500

    Update imports for router-dom in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom';
+// import reactLogo from './assets/react.svg' - need new logo?
+// import viteLogo from '/vite.svg' - need new logo?
 import './App.css'
 
 function App() {
