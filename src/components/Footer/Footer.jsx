9d7f5f7e303647724168d1f15ceacade24f3e958
Janet Webster <twixmixy@gmail.com>
2023-12-02 14:22:35 -0500

    Add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MDBFooter, MDBIcon } from 'mdb-react-ui-kit';
+
+const Footer = () => {
+    return (
+        <MDBFooter className="text-center text-lg-start text-muted" bgColor='light'>
+            <div className="text-center p-4" style={{ backgroundColor: 'rgba(0,0,0,0.05' }}>
+                <span>
+                    
+                </span>
+            </div>
+        </MDBFooter>
+    )
+};
