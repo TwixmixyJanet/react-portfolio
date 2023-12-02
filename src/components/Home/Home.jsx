ca87b73136d3b74e13e2950c93cd1568d2d0e87b
Janet Webster <twixmixy@gmail.com>
2023-12-02 15:36:47 -0500

    Update splash image path

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MDBContainer } from 'mdb-react-ui-kit';
-import splash from '../../assets/splash.png';
+import splash from '../../assets/images/splash.jpg';
 
 const Home = () => {
     return (
