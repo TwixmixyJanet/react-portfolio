6914db30ad12f35de61429bd34346b971476212d
Janet Webster <twixmixy@gmail.com>
2023-12-02 14:43:06 -0500

    Add Home component to React portfolio

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MDBContainer } from 'mdb-react-ui-kit';
+import splash from '../../assets/splash.png';
+
+const Home = () => {
+    return (
+        <MDBContainer fluid className="text-center vh-100 bkgrd">
+            <img src={splash} alt='splash' className='splash img-fluid rounded-pill shadow-4-strong'/>
+            <p className="my-0 py-0 text-dark fs-1">My React Portfolio!</p>
+        </MDBContainer>
+    )
+};
+
+export default Home;
