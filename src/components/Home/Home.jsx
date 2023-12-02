9db44c7a094c345ea6c758fade795695d6cddac3
Janet Webster <twixmixy@gmail.com>
2023-12-02 17:05:29 -0500

    Update background gradients in CSS files

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import { MDBContainer } from 'mdb-react-ui-kit';
 import splash from '../../assets/images/splash.jpg';
 
 const Home = () => {
     return (
-        <MDBContainer fluid className="text-center vh-100 bkgrd">
+        <MDBContainer fluid className="text-center bkgrd">
             <img src={splash} alt='splash' className='splash img-fluid rounded-pill shadow-4-strong'/>
-            <p className="my-0 py-0 text-dark fs-1">My React Portfolio!</p>
         </MDBContainer>
     )
 };
