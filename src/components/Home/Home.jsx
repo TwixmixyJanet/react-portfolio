99ac0052bce0094a75785db81211edd4cabbfd1e
Janet Webster <twixmixy@gmail.com>
2023-12-03 18:10:47 -0500

    Update Home component styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <MDBContainer fluid className="text-center bkgrd">
             <div>
-                <h1 className="display-1 text-light">Janet Webster</h1>
+                <h1 className="display-1 text-light p-4">Janet Webster</h1>
                 <h2 className="display-5 text-light">Full Stack MERN Software Engineer</h2>
             </div>
             <img src={splash} alt='splash' className='splash img-fluid rounded-pill shadow-4-strong
