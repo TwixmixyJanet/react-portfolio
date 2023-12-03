2bd536d216b00729d23d5add93d14abebc2086a2
Janet Webster <twixmixy@gmail.com>
2023-12-02 19:37:18 -0500

    Update navigation and portfolio components

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { 
     MDBContainer,
     MDBRow,
@@ -42,15 +41,15 @@ const Portfolio = () => {
                     <MDBCard className="h-100">
                         <MDBCardImage className="img-thumbnail" position='top' src={project2}/>
                         <MDBCardBody>
-                            <MDBCardTitle>Project 2</MDBCardTitle>
+                            <MDBCardTitle>Qif - A Quote & Giphy Generator</MDBCardTitle>
                             <MDBCardText>
-                                <p>Project 2 description</p>
-                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                                <p>This is a quote and giphy generator based off a keyword search</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, Tailwind CSS, Granim, APIs, Git, VSCode</p>
                             </MDBCardText>
                         </MDBCardBody>
                         <MDBCardFooter className="text-center">
-                            <MDBCardLink href=''>Repo Link</MDBCardLink>
-                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                            <MDBCardLink href='https://github.com/TwixmixyJanet/Qif-Generator'>Repo Link</MDBCardLink>
+                            <MDBCardLink href='https://twixmixyjanet.github.io/Qif-Generator/'>Deploy Link</MDBCardLink>
                         </MDBCardFooter>
                     </MDBCard>
                 </MDBCol>
@@ -58,15 +57,15 @@ const Portfolio = () => {
                     <MDBCard className="h-100">
                         <MDBCardImage className="img-thumbnail" position='top' src={project3}/>
                         <MDBCardBody>
-                            <MDBCardTitle>Project 3</MDBCardTitle>
+                            <MDBCardTitle>Full Stack Community Blog</MDBCardTitle>
                             <MDBCardText>
-                                <p>Project 3 description</p>
-                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                                <p>This is a full stack blog built in the content management essentials that allows users to login, create, manage, and delete articles.</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, Tailwind CSS, Granim, APIs, Git, VSCode</p>
                             </MDBCardText>
                         </MDBCardBody>
                         <MDBCardFooter className="text-center">
-                            <MDBCardLink href=''>Repo Link</MDBCardLink>
-                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                            <MDBCardLink href='https://github.com/TwixmixyJanet/tech-blog'>Repo Link</MDBCardLink>
+                            <MDBCardLink href='https://janets-tech-blog-a73a7af9497f.herokuapp.com/'>Deploy Link</MDBCardLink>
                         </MDBCardFooter>
                     </MDBCard>
                 </MDBCol>
@@ -74,15 +73,15 @@ const Portfolio = () => {
                     <MDBCard className="h-100">
                         <MDBCardImage className="img-thumbnail" position='top' src={project4}/>
                         <MDBCardBody>
-                            <MDBCardTitle>Project 4</MDBCardTitle>
+                            <MDBCardTitle>Weather Dashboard</MDBCardTitle>
                             <MDBCardText>
-                                <p>Project 4 description</p>
-                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                                <p>This is a JavaScript, API and Bootstrap focused project with the goal to create a weather dashboard app</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs, DayJS, Bootstrap, GitHub Pages</p>
                             </MDBCardText>
                         </MDBCardBody>
                         <MDBCardFooter className="text-center">
-                            <MDBCardLink href=''>Repo Link</MDBCardLink>
-                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                            <MDBCardLink href='https://github.com/TwixmixyJanet/weather-dashboard'>Repo Link</MDBCardLink>
+                            <MDBCardLink href='https://twixmixyjanet.github.io/weather-dashboard/'>Deploy Link</MDBCardLink>
                         </MDBCardFooter>
                     </MDBCard>
                 </MDBCol>
@@ -90,15 +89,15 @@ const Portfolio = () => {
                     <MDBCard className="h-100">
                         <MDBCardImage className="img-thumbnail" position='top' src={project5}/>
                         <MDBCardBody>
-                            <MDBCardTitle>Project 5</MDBCardTitle>
+                            <MDBCardTitle>Interactive Timed Quiz</MDBCardTitle>
                             <MDBCardText>
-                                <p>Project 5 description</p>
-                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                                <p>This project is a JavaScript focused application with the goal to create a timed quiz that will keep score based on the results</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, APIs, GitHub Pages</p>
                             </MDBCardText>
                         </MDBCardBody>
                         <MDBCardFooter className="text-center">
-                            <MDBCardLink href=''>Repo Link</MDBCardLink>
-                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                            <MDBCardLink href='https://github.com/TwixmixyJanet/code-quiz'>Repo Link</MDBCardLink>
+                            <MDBCardLink href='https://twixmixyjanet.github.io/code-quiz/'>Deploy Link</MDBCardLink>
                         </MDBCardFooter>
                     </MDBCard>
                 </MDBCol>
@@ -106,15 +105,15 @@ const Portfolio = () => {
                     <MDBCard className="h-100">
                         <MDBCardImage className="img-thumbnail" position='top' src={project6}/>
                         <MDBCardBody>
-                            <MDBCardTitle>Project 6</MDBCardTitle>
+                            <MDBCardTitle>Fully Flex Portfolio</MDBCardTitle>
                             <MDBCardText>
-                                <p>Project 6 description</p>
-                                <p>Technologies used: HTML, CSS, JavaScript, jQuery, APIs</p>
+                                <p>A front end portfolio application with all from-scratch code and no CDNs or NPMs</p>
+                                <p>Technologies used: HTML, CSS, JavaScript, GitHub Pages, VSCode, Web Accessibiity</p>
                             </MDBCardText>
                         </MDBCardBody>
                         <MDBCardFooter className="text-center">
-                            <MDBCardLink href=''>Repo Link</MDBCardLink>
-                            <MDBCardLink href=''>Deploy Link</MDBCardLink>
+                            <MDBCardLink href='https://github.com/TwixmixyJanet/janet-webster-portfolio'>Repo Link</MDBCardLink>
+                            <MDBCardLink href='https://twixmixyjanet.github.io/janet-webster-portfolio/'>Deploy Link</MDBCardLink>
                         </MDBCardFooter>
                     </MDBCard>
                 </MDBCol>
