e770581a5cec5e8bf69a0eec14f2667803119c9c
Janet Webster <twixmixy@gmail.com>
2023-12-03 17:55:06 -0500

    Update image class in Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,8 @@ const Home = () => {
                 <h1 className="display-1 text-light">Janet Webster</h1>
                 <h2 className="display-5 text-light">Full Stack MERN Software Engineer</h2>
             </div>
-            <img src={splash} alt='splash' className='splash img-fluid rounded-pill shadow-4-strong my-4'/>
+            <img src={splash} alt='splash' className='splash img-fluid rounded-pill shadow-4-strong
+            card-picture my-5'/>
         </MDBContainer>
     )
 };
