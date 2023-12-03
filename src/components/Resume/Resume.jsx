97b69ea0e00c537f0d4282522d838f3c2cc9da6e
Janet Webster <twixmixy@gmail.com>
2023-12-02 23:34:48 -0500

    Update resume URL

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -11,7 +11,7 @@ import {
 import resume from '../../assets/images/resume.png';
 
 const Resume = () => {
-    const resumeUrl = '/assets/PortfolioResume.pdf';
+    const resumeUrl = 'https://drive.google.com/file/d/1fyk6UEzhQw5PtoAYv6Ymtxsua_sFBljL/view?usp=sharing';
     return (
         <MDBContainer fluid className='bkgrd'>
             <div className="d-flex justify-content-center">
