e6caa3b27046a04dabd491f12687856662317b93
Janet Webster <twixmixy@gmail.com>
2023-12-03 19:30:33 -0500

    Update fontawesome-free to version 6.5.1

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import './App.css'
 
 import { Outlet } from 'react-router-dom';
 
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 function App() {
 
   return (
