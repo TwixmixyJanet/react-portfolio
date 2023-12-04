16f5f845937971782d7854046c7ca2712f031214
Janet Webster <twixmixy@gmail.com>
2023-12-03 19:43:57 -0500

    Update social media icons in Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
         <MDBFooter className="text-center text-lg-start text-muted" bgColor='light'>
             <div className="text-center p-4" >
                     <a rel='noreferrer' href="https://www.linkedin.com/in/twixmixy/" className="me-4 text-reset" target='_blank'>
-                        LinkedIn<MDBIcon fab icon='linkedin' size='2x'/>
+                        <MDBIcon fab icon='linkedin' size='2x'/>
                     </a>
                     <a rel='noreferrer' href="https://github.com/TwixmixyJanet" className="me-4 text-reset" target='_blank'>
-                        GitHub<MDBIcon fab icon='github' size='2x'/>
+                        <MDBIcon fab icon='github' size='2x'/>
                     </a>
                     <a rel='noreferrer' href="https://twixmixy.substack.com/" className="me-4 text-reset" target='_blank'>
-                        Substack<MDBIcon fab icon='newspaper' size='2x'/>
+                        <MDBIcon fab icon='microblog' size='2x'/>
                     </a>
             </div>
         </MDBFooter>
