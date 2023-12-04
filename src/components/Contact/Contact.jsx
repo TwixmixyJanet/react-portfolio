ab0c54feba250b029403033217a8ef7ff16e76c5
Janet Webster <twixmixy@gmail.com>
2023-12-03 20:41:37 -0500

    Update Contact form layout and add MDBInput components

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { MDBContainer } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBInput } from 'mdb-react-ui-kit';
 import './Contact.css';
 import { useForm } from 'react-hook-form';
 
@@ -27,26 +27,42 @@ const Contact = () => {
     return (
         <MDBContainer fluid className='vh-100 bkgrd'>
             <div className="d-flex w-100 justify-content-center">
-                <form onSubmit={handleSubmit(onSubmit)} className="w-25 mt-4">
-                    <p className="my-0 text-light">Name</p>
-                    <input defaultValue={initialValues.senderName}
+                <form onSubmit={handleSubmit(onSubmit)} className="w-50 mt-4">
+                    {/* <p className="my-0 text-light">Name</p> */}
+                    <MDBInput label='Name' id='name' type='text' size='lg' contrast
+                    defaultValue={initialValues.senderName}
                     {...register("senderName", { required: true, minLength: 2, maxLength: 80 })}
-                    className="w-100" />
-                    {errors.senderName && <p className="text-danger mw-100">Name is required</p>}
-                    <p className="my-0 text-light">Email</p>
-                    <input defaultValue={initialValues.email}
+                    className="w-100 m-4" />
+                    {/* <input 
+                    defaultValue={initialValues.senderName}
+                    {...register("senderName", { required: true, minLength: 2, maxLength: 80 })}
+                    className="w-100" /> */}
+                    {errors.senderName && <p className="text-warning mw-100">Name is required</p>}
+                    {/* <p className="my-0 text-light">Email</p> */}
+                    <MDBInput label='Email' id='email' type='email' size='lg' contrast
+                    defaultValue={initialValues.email}
+                    {...register("email", { 
+                        required: true, 
+                        pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    })} 
+                    className="w-100 m-4" />
+                    {/* <input defaultValue={initialValues.email}
                     {...register("email", { 
                         required: true, 
                         pattern: /^\S+@\S+$/i 
                         // potential alternative pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                     })}
-                    className='w-100' />
-                    {errors.email && <p className="text-danger mw-100">Email is required</p>}
-                    <p className="my-0 text-light">Message</p>
-                    <input defaultValue={initialValues.message}
+                    className='w-100' /> */}
+                    {errors.email && <p className="text-warning mw-100">Email is required</p>}
+                    {/* <p className="my-0 text-light">Message</p> */}
+                    <MDBInput label='Message' id='message' type='text' size='lg' contrast
+                    defaultValue={initialValues.message}
                     {...register("message", { required: true })}
-                    className="w-100" />
-                    {errors.message && <p className="text-danger mw-100">Message is required</p>}
+                    className="w-100 m-4" />
+                    {/* <input defaultValue={initialValues.message}
+                    {...register("message", { required: true })}
+                    className="w-100" /> */}
+                    {errors.message && <p className="text-warning mw-100">Message is required</p>}
                     <input type="submit" className="btn btn-primary mt-3" />
                 </form>
             </div>
