5492c3ba198cd2bd28995755840f95f72a4eca19
Janet Webster <twixmixy@gmail.com>
2023-12-04 20:25:23 -0500

    Adding code comments to app and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
+// Import the custom Navigation component
 import MyNav from './components/Navigation';
-import Footer from './components/Footer';
-import './App.css'
 
+// Import the Footer component
+import Footer from './components/Footer';
+
+// Import the styling for the App component
+import './App.css';
+
+// Import the Outlet component from 'react-router-dom', which is used to render child components based on the current route
 import { Outlet } from 'react-router-dom';
 
+// Import styles from 'mdb-react-ui-kit' and Font Awesome icons
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+// Define the main App component
 function App() {
-
+  // Render the main structure of the application
   return (
     <>
+        {/* Render the custom Navigation component */}
         <MyNav />
+
+        {/* Main content area, renders child components based on the current route */}
         <main>
           <Outlet />
         </main>
+
+        {/* Render the Footer component */}
         <Footer />
     </>
-  )
+  );
 }
 
+// Export the App component as the default export
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,55 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Error from './components/Error';
-import Home from './components/Home';
-import Contact from './components/Contact';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
+// Import the necessary modules and components from external libraries and files
+import ReactDOM from 'react-dom/client'; // ReactDOM for rendering React components into the DOM
+import App from './App'; // Import the main App component
+import './index.css'; // Import the styles defined in the index.css file
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'; // Import styles from the 'mdb-react-ui-kit' library
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // Import routing-related components from 'react-router-dom'
+import Error from './components/Error'; // Import the Error component
+import Home from './components/Home'; // Import the Home component
+import Contact from './components/Contact'; // Import the Contact component
+import About from './components/About'; // Import the About component
+import Portfolio from './components/Portfolio'; // Import the Portfolio component
+import Resume from './components/Resume'; // Import the Resume component
 
+// Create a router using the createBrowserRouter function from 'react-router-dom'
 const router = createBrowserRouter([
   {
+    // Define the main route for the root path '/'
     path: '/',
-    element: <App />,
-    errorElement: <Error />,
+    element: <App />, // Render the main App component for the root path
+    errorElement: <Error />, // Render the Error component in case of an error
     children: [
       {
+        // Define a child route for the root path, indicating it as the index
         index: true,
-        element: <Home />,
+        element: <Home />, // Render the Home component for the root path
       },
       {
+        // Define a child route for the '/About' path
         path: '/About',
-        element: <About />,
+        element: <About />, // Render the About component for the '/About' path
       },
       {
+        // Define a child route for the '/Portfolio' path
         path: '/Portfolio',
-        element: <Portfolio />,
+        element: <Portfolio />, // Render the Portfolio component for the '/Portfolio' path
       },
       {
+        // Define a child route for the '/Contact' path
         path: '/Contact',
-        element: <Contact />,
+        element: <Contact />, // Render the Contact component for the '/Contact' path
       },
       {
+        // Define a child route for the '/Resume' path
         path: '/Resume',
-        element: <Resume />,
+        element: <Resume />, // Render the Resume component for the '/Resume' path
       },
     ],
   },
 ]);
 
+// Use ReactDOM.createRoot to create a root instance for rendering React elements
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Render the RouterProvider component with the specified router configuration
   <RouterProvider router={router} />
 );
