8d2e5cf8eb86151b14d8e39876b4646b37fb3f56
Janet Webster <twixmixy@gmail.com>
2023-12-04 20:53:47 -0500

    comments for mynav component

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -13,53 +13,72 @@ import {
 import { LinkContainer } from 'react-router-bootstrap';
 import './MyNav.css';
 
+// Functional component representing the navigation bar of the application
 const MyNav = () => {
+    // State to manage the visibility of the navigation bar on small screens
     const [showNavNoToggler, setShowNavNoToggler] = useState(false);
 
     return (
         <>
-        <MDBNavbar expand='md' dark className='p-5'>
-            <MDBContainer className='d-flex flex-wrap justify-content-center align-items-center'>
-                <MDBNavbarToggler type='button'
-                data-target='#navbar'
-                aria-controls='navbar'
-                aria-expanded='false'
-                aria-label='Toggle navigation'
-                onClick={() => setShowNavNoToggler(!showNavNoToggler)}>
-                    <MDBIcon fas icon='bars' size='2x' />
-                </MDBNavbarToggler>
-                <MDBCollapse navbar open={showNavNoToggler}>
-                <MDBNavbarNav className='d-flex flex-wrap align-items-center'>
-                    <LinkContainer to='/'>
-                        <MDBNavbarBrand href="#home" >Janet Webster</MDBNavbarBrand>
-                    </LinkContainer>
-                    <LinkContainer to='/about' className='p-2'>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='#about'>About</MDBNavbarLink>
-                        </MDBNavbarItem>
-                        
-                    </LinkContainer>
-                    <LinkContainer to='/portfolio' className='p-2'>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='#portfolio'>Portfolio</MDBNavbarLink>
-                        </MDBNavbarItem>
-                    </LinkContainer>
-                    <LinkContainer to='/resume' className='p-2'>
-                    <MDBNavbarItem>
-                            <MDBNavbarLink href='#resume'>Résumé</MDBNavbarLink>
-                        </MDBNavbarItem>
-                    </LinkContainer>
-                    <LinkContainer to='/contact' className='p-2'>
-                    <MDBNavbarItem>
-                            <MDBNavbarLink href='#contact'>Contact</MDBNavbarLink>
-                        </MDBNavbarItem>
-                    </LinkContainer>
-                </MDBNavbarNav>
-                </MDBCollapse>
-            </MDBContainer>
-        </MDBNavbar>
+            {/* MDBNavbar component with dark styling and padding */}
+            <MDBNavbar expand='md' dark className='p-5'>
+                {/* MDBContainer for centering and aligning items in the navigation bar */}
+                <MDBContainer className='d-flex flex-wrap justify-content-center align-items-center'>
+                    {/* MDBNavbarToggler for the collapsible navigation menu on small screens */}
+                    <MDBNavbarToggler
+                        type='button'
+                        data-target='#navbar'
+                        aria-controls='navbar'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                        onClick={() => setShowNavNoToggler(!showNavNoToggler)}>
+                        {/* Icon for the toggle button */}
+                        <MDBIcon fas icon='bars' size='2x' />
+                    </MDBNavbarToggler>
+
+                    {/* MDBCollapse component for the collapsible navigation menu */}
+                    <MDBCollapse navbar open={showNavNoToggler}>
+                        {/* MDBNavbarNav for the navigation items */}
+                        <MDBNavbarNav className='d-flex flex-wrap align-items-center'>
+                            {/* Home link with a LinkContainer for routing */}
+                            <LinkContainer to='/'>
+                                <MDBNavbarBrand href="#home" >Janet Webster</MDBNavbarBrand>
+                            </LinkContainer>
+
+                            {/* About link with a LinkContainer for routing */}
+                            <LinkContainer to='/about' className='p-2'>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#about'>About</MDBNavbarLink>
+                                </MDBNavbarItem>
+                            </LinkContainer>
+
+                            {/* Portfolio link with a LinkContainer for routing */}
+                            <LinkContainer to='/portfolio' className='p-2'>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#portfolio'>Portfolio</MDBNavbarLink>
+                                </MDBNavbarItem>
+                            </LinkContainer>
+
+                            {/* Résumé link with a LinkContainer for routing */}
+                            <LinkContainer to='/resume' className='p-2'>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#resume'>Résumé</MDBNavbarLink>
+                                </MDBNavbarItem>
+                            </LinkContainer>
+
+                            {/* Contact link with a LinkContainer for routing */}
+                            <LinkContainer to='/contact' className='p-2'>
+                                <MDBNavbarItem>
+                                    <MDBNavbarLink href='#contact'>Contact</MDBNavbarLink>
+                                </MDBNavbarItem>
+                            </LinkContainer>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBContainer>
+            </MDBNavbar>
         </>
     );
 }
 
+// Export the 'MyNav' component
 export default MyNav;
