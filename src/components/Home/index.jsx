060047f3e1e35a3d9725259ce76a749ff084742f
Janet Webster <twixmixy@gmail.com>
2023-12-04 20:51:48 -0500

    comments for home component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,17 +1,24 @@
 import { MDBContainer } from 'mdb-react-ui-kit';
 import splash from '../../assets/images/splash.jpg';
 
+// Functional component representing the home page of the application
 const Home = () => {
     return (
+        // MDBContainer component for a fluid container with a custom background class ('bkgrd')
         <MDBContainer fluid className="text-center bkgrd">
             <div>
+                {/* Display the name in a large font size with light text color */}
                 <h1 className="display-1 text-light p-4">Janet Webster</h1>
+
+                {/* Display the job title in a smaller font size with light text color */}
                 <h2 className="display-5 text-light">Full Stack MERN Software Engineer</h2>
             </div>
-            <img src={splash} alt='splash' className='splash img-fluid rounded-pill shadow-4-strong
-            card-picture my-5'/>
+
+            {/* Display an image with a rounded-pill style, shadow, and margin */}
+            <img src={splash} alt='splash' className='splash img-fluid rounded-pill shadow-4-strong card-picture my-5'/>
         </MDBContainer>
-    )
+    );
 };
 
+// Export the 'Home' component
 export default Home;
